c810de31d279ac97bb6c49ff3ae96f3fa21b840e
Charan K <132739895+ck2311@users.noreply.github.com>
2023-10-09 20:41:06 +0530

    Add files via upload

---
+++ JAVA Script/C1.js
@@ -0,0 +1,9 @@
+empname = Sai Charan K
+console.log empname, typeof(Sai Charan K)
+
+emp = 20000
+console.log empname, typeof(empsalary)
+etp.log empsalary, typeof(empsalary)
+
+etp.leave = false
+console.log etp
