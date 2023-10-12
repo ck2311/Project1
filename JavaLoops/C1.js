46e21ce49e5a9baa8147ba0ef437c1e26f40c5da
ck2311 <saicharankoppala@gmail.com>
2023-10-12 13:02:34 +0530

    v1

---
+++ JavaLoops/C1.js
@@ -0,0 +1,20 @@
+/*Else if loop*/
+var userSalary = 60000
+if (userSalary == 10000) {
+    console.log("userSalary is", userSalary)
+}
+else if (userSalary == 20000) {
+    console.log("userSalary is", userSalary)
+}
+if (userSalary == 30000) {
+    console.log("userSalary is", userSalary)
+}
+else if (userSalary == 40000) {
+    console.log("userSalary is", userSalary)
+}
+if (userSalary == 50000) {
+    console.log("userSalary is", userSalary)
+}
+else{
+    console.log("userSalary is not validated", userSalary)
+}
